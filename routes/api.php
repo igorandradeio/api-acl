ebaef7b113b4db313e0243b62a1662f69d8f4207
Igor Andrade <oigorwebs@gmail.com>
2022-01-18 16:18:47 -0300

    Add user routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,34 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\{
+    CategoryController,
+    ProductController,
+    ResourceController,
+    PermissionUserController,
+    UserController
+};
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return response()->json(['message' => 'Hello world']);
 });
+
+/** User permission route*/
+Route::get('/users/permission', [PermissionUserController::class, 'permissionUser']);
+/** User routes*/
+Route::apiResource('/users', UserController::class);
+
+Route::apiResource('/categories/{category}/products', ProductController::class);
+/** Update category by uuid*/
+Route::put('/categories/{uuid}', [CategoryController::class, 'update']);
+/** Delete category by uuid*/
+Route::delete('/categories/{uuid}', [CategoryController::class, 'destroy']);
+/** Get category by uuid*/
+Route::get('/categories/{uuid}', [CategoryController::class, 'show']);
+/** Create category */
+Route::post('/categories', [CategoryController::class, 'store']);
+/** Get all categories */
+Route::get('/categories', [CategoryController::class, 'index']);
+/** Get all resources */
+Route::get('/resources', [ResourceController::class, 'index']);
