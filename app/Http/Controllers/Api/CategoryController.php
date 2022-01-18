1859382223dc1e356e40a8e542907689e36cdbbf
Igor Andrade <oigorwebs@gmail.com>
2022-01-18 08:52:55 -0300

    Create category controller

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateCategory;
+use App\Http\Resources\CategoryResource;
+use App\Services\CategoryService;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    protected $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = $this->categoryService->getCategories();
+
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUpdateCategory $request)
+    {
+        $category = $this->categoryService->createNewCategory($request->validated());
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function show($uuid)
+    {
+        $category = $this->categoryService->getCategory($uuid);
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreUpdateCategory $request, $uuid)
+    {
+        $this->categoryService->updateCategory($uuid, $request->validated());
+
+        return response()->json(['message' => 'updated']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  string  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($uuid)
+    {
+        $category = $this->categoryService->deleteCategory($uuid);
+
+        return response()->json([], 204);
+    }
+}
