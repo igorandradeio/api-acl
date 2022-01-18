d8170b23f91ccecfdc00b54b7533ea6de415653a
Igor Andrade <oigorwebs@gmail.com>
2022-01-18 16:18:37 -0300

    Create user controller

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateUser;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = $this->userService->getUsers();
+
+        return UserResource::collection($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUpdateUser $request)
+    {
+        $data = $request->validated();
+
+        $data['password'] = bcrypt($data['password']);
+
+        $user = $this->userService->createNewUser($data);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $identify
+     * @return \Illuminate\Http\Response
+     */
+    public function show($identify)
+    {
+        $user = $this->userService->getUserByUuid($identify);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $identify
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreUpdateUser $request, $identify)
+    {
+        $data = $request->validated();
+
+        $this->userService->updateUserByUuid($identify, $data);
+
+        return response()->json([
+            'updated' => 'success'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  string  $identify
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($identify)
+    {
+        $this->userService->deleteUserByUuid($identify);
+
+        return response()->json([], 204);
+    }
+}
