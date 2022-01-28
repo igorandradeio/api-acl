4ba5d75d19759ff0e639918923a884801b71375e
Igor <oigorwebs@gmail.com>
2022-01-28 16:41:47 -0300

    Add auth and login routes

--- routes/api.php
+++ routes/api.php
@@ -7,28 +7,45 @@ use App\Http\Controllers\Api\{
     PermissionUserController,
     UserController
 };
+use App\Http\Controllers\Api\Auth\{
+    AuthController,
+    RegisterController
+};
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return response()->json(['message' => 'Hello world']);
 });
+/**
+ * Auth and Register routes
+ */
+Route::post('/register', [RegisterController::class, 'store']);
 
-/** User permission route*/
-Route::get('/users/permission', [PermissionUserController::class, 'permissionUser']);
-/** User routes*/
-Route::apiResource('/users', UserController::class);
 
-Route::apiResource('/categories/{category}/products', ProductController::class);
-/** Update category by uuid*/
-Route::put('/categories/{uuid}', [CategoryController::class, 'update']);
-/** Delete category by uuid*/
-Route::delete('/categories/{uuid}', [CategoryController::class, 'destroy']);
-/** Get category by uuid*/
-Route::get('/categories/{uuid}', [CategoryController::class, 'show']);
-/** Create category */
-Route::post('/categories', [CategoryController::class, 'store']);
-/** Get all categories */
-Route::get('/categories', [CategoryController::class, 'index']);
-/** Get all resources */
-Route::get('/resources', [ResourceController::class, 'index']);
+/** User login */
+Route::post('/auth', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    /** User permission route*/
+    Route::get('/users/permission', [PermissionUserController::class, 'permissionUser']);
+    /** User routes*/
+    Route::apiResource('/users', UserController::class);
+
+    Route::apiResource('/categories/{category}/products', ProductController::class);
+    /** Update category by uuid*/
+    Route::put('/categories/{uuid}', [CategoryController::class, 'update']);
+    /** Delete category by uuid*/
+    Route::delete('/categories/{uuid}', [CategoryController::class, 'destroy']);
+    /** Get category by uuid*/
+    Route::get('/categories/{uuid}', [CategoryController::class, 'show']);
+    /** Create category */
+    Route::post('/categories', [CategoryController::class, 'store']);
+    /** Get all categories */
+    Route::get('/categories', [CategoryController::class, 'index']);
+    /** Get all resources */
+    Route::get('/resources', [ResourceController::class, 'index']);
+});
