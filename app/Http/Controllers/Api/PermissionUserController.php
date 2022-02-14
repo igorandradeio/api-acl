3faf07353f86aeba86468089e992a67a3df8ad53
Igor <oigorwebs@gmail.com>
2022-02-14 11:52:56 -0300

    Add PermissionsUser

--- app/Http/Controllers/Api/PermissionUserController.php
+++ app/Http/Controllers/Api/PermissionUserController.php
@@ -3,19 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AddPermissionUser;
 use App\Http\Resources\PermissionResource;
+use App\Services\UserService;
+use Illuminate\Cache\Repository;
 use Illuminate\Http\Request;
 
 class PermissionUserController extends Controller
 {
 
-    public function permissionUser($id)
+    protected $userService;
+
+    public function __construct(UserService $userService)
     {
-        $user = $this->user
-            ->where('uuid', $id)
-            ->with('permissions')
-            ->firstOrFail();
+        $this->userService = $userService;
+    }
+
+    public function permissionUser($uuid)
+    {
+        $user = $this->userService->getUserByUuid($uuid);
 
         return PermissionResource::collection($user->permissions);
+    }
+
+    public function addPermissionsUser(AddPermissionUser $request)
+    {
+        $user = $this->userService->getUserByUuid($request->user);
+
+        $user->permissions()->attach($request->permissions);
+
+        return response()->json(['message' => 'success']);
     }
 }
